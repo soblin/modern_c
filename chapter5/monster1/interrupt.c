f606b63101142ae97251d5b52cf03a9dd71b62c0
soblin <hilo.mamoru@gmail.com>
2019-03-19 12:26:39 +0900

    ended refactoring

--- chapter5/monster1/interrupt.c
+++ chapter5/monster1/interrupt.c
@@ -1,71 +1,104 @@
 #include "hw.h"
 #include "interrupt.h"
 
-void lowPriorityISRCode(void){
-  if(PIR1bits.RCIF){
-    if(RCSTAbits.OERR){
-      int c;
-      // ポートをリセット
-      RCSTAbits.CREN = 0;
-      c = USART_RECEIVE_CHAR();
-      RCSTAbits.CREN = 1;
-    }
-    else{
-      int c = USART_RECEIVE_CHAR();
-      PUT_CHAR_TO_RING_BUFFER(&ringBufferForReceive, (char)c);
-    }
+unsigned int isUsartInterruptOccurred(void){
+  return PIR1bits.RCIF;
+}
+
+unsigned int isUsartOverrunOccurred(void){
+  return RCSTAbits.OERR;
+}
+
+int isTimer3Overflow(void){
+  return PIR2bits.TMR3IF != 0;
+}
+
+void disableTime3Interrupt(){
+  PIE2bits.TMR3IE = 0;
+}
+
+void resetUserPort(void){
+  // ポートをリセット
+  RCSTAbits.CREN = 0;
+  int c = USART_RECEIVE_CHAR();
+  RCSTAbits.CREN = 1;
+}
+
+void onUsartInterrupt(void){
+  if(isUsartOverrunOccurred()){
+    resetUserPort();
   }
-  else if(PIR2bits.TMR3IF != 0){
-    int c;
-    if((c = getCharFromRingBuffer(&ringBufferForLcd)) != -1){
-      if(c == (BYTE)0xFE){
-        c = getCharFromRingBuffer(&ringBufferForLcd);
+  else{
+    int c = USART_RECEIVE_CHAR();
+    PUT_CHAR_TO_RING_BUFFER(&ringBufferForReceive, (char)c);
+  }  
+}
 
-        PORTBbits.RB0 = 0;
-        PORTBbits.RB2 = 0;
-        PORTA = c >> 4;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 0;
-        PORTBbits.RB1 = 0;
+void write1byteToLcd(int c){
+  PORTBbits.RB2 = 0;
+  PORTA = c >> 4;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 0;
+  PORTBbits.RB1 = 0;
 
-        PORTA = c & 0xf;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 0;
+  PORTA = c & 0xf;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 1;
+  PORTBbits.RB1 = 0;  
+}
 
-        PIR2bits.TMR3IF = 0;
-        TMR3H = (65467u >> 8);
-        TMR3L = (65467u & 0xff);
-      }
-      else{
-        PORTBbits.RB0 = 1;
-        PORTBbits.RB2 = 0;
-        PORTA = c >> 4;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 0;
-        PORTBbits.RB1 = 0;
+void performLcdControl(int c){
+  c = getCharFromRingBuffer(&ringBufferForLcd);
 
-        PORTA = c & 0xf;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 1;
-        PORTBbits.RB1 = 0;
+  PORTBbits.RB0 = 0; // control mode
+  write1byteToLcd(c);
+  
+  PIR2bits.TMR3IF = 0;
+  TMR3H = (65467u >> 8);
+  TMR3L = (65467u & 0xff);  
+}
 
-        PIR2bits.TMR3IF = 0;
-        TMR3H = (65467u >> 8);
-        TMR3L = (65467u & 0xff);        
-      }
-    }
-    else{
-      PIE2bits.TMR3IE = 0;
-    }
+void performLcdData(int c){
+  PORTBbits.RB0 = 1; // data mode
+
+  write1byteToLcd(c);
+    
+  PIR2bits.TMR3IF = 0;
+  TMR3H = (65467u >> 8);
+  TMR3L = (65467u & 0xff);  
+}
+
+void processLcdData(int c){
+  if(c == (BYTE)0xFE){
+    performLcdControl(c);
+  }
+  else{
+    performLcdData(c);
   }
 }
+
+void onTimer3Interrupt(void){
+  int c;
+  if((c = getCharFromRingBuffer(&ringBufferForLcd)) != -1){
+    processLcdData(c);
+  }
+  else{
+    disableTime3Interrupt();
+  }
+}
+
+void lowPriorityISRCode(void){
+  if(isUsartInterruptOccurred()){
+    onUsartInterrupt();
+  }
+  else if(isTimer3Overflow()){
+    onTimer3Interrupt();
+  }
+}
+
 
 int main(){
   return 0;
